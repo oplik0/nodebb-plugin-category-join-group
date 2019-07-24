d551921569bf4c896a56a8bf8bb75d3170dfd7b4
Jakub <25460763+oplik0@users.noreply.github.com>
2019-07-24 17:20:40 +0000

    Rename some more things

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 	$(window).on('action:ajaxify.end', function() {
 
 		if (app.template === 'category' && app.user.uid) {
-			var leaveHtml = '<button type="button" class="btn btn-default btn-warning leave"><i class="fa fa-pencil"></i> [[categorynotifications:leave]]</button>';
-			var joinHtml = '<button type="button" class="btn btn-default btn-success join"><i class="fa fa-pencil"></i> [[categorynotifications:join]]</button>';
+			var leaveHtml = '<button type="button" class="btn btn-default btn-warning leave"><i class="fa fa-pencil"></i> [[categoryjoingroup:leave]]</button>';
+			var joinHtml = '<button type="button" class="btn btn-default btn-success join"><i class="fa fa-pencil"></i> [[categoryjoingroup:join]]</button>';
 
 			var cid = ajaxify.data.cid;
 			require(['translator'], function (translator) {
-				socket.emit('plugins.categoryNotifications.isMember', {cid: cid}, function(err, isMember) {
+				socket.emit('plugins.categoryJoinGroup.isMember', {cid: cid}, function(err, isMember) {
 
 					function handleClick(className, method) {
 						$('.category').on('click', className, function() {
